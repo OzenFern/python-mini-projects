1529a7455134472db7213ce805fdf55a4eb40d12
OzenFern <ozenfernandes@gmail.com>
2026-03-14 22:09:39 +0530

    feat: add get_input() and get_bid_amt()

--- blind-auction/main.py
+++ blind-auction/main.py
@@ -1,10 +1,30 @@
 from ascii_art import logo
 import os
 
+
 # Fucntion Definition
 def start_auction():
     os.system("cls" if os.name == "nt" else "clear")
     print(logo)
 
+
+def get_input(prompt):
+    return input(prompt).strip()
+
+
+def get_bid_amt():
+    while True:
+        try:
+            bid_amt = int(get_input("Enter your bid: $"))
+            if bid_amt < 0:
+                print("Please enter a positive bid")
+                continue
+            return bid_amt
+        except ValueError:
+            print("Please enter a number")
+
+
 # Driver
 start_auction()
+name = get_input("Enter your name: ")
+get_bid_amt()
