2ff35f21f1ab46ae09b691d5550f353aef0c1974
OzenFern <ozenfernandes@gmail.com>
2026-05-18 12:26:05 +0530

    Add display_tracks function

--- music-app/main.py
+++ music-app/main.py
@@ -1,3 +1,15 @@
 from ascii_art import logo
+from albums import tracks
 
+
+def display_tracks():
+    for i, band in enumerate(tracks, start=1):
+        print(f"{i}. {band[0]}")
+        for j, song in enumerate(band[1], start=1):
+            print(f"\t{j}. {song}")
+        print()
+
+
+# Driver Code
 print(logo)
+display_tracks()
