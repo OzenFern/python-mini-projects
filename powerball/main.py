20ca4f1ab2c2e36a4a0bc58f3cb21c48ce6d0ff4
OzenFern <ozenfernandes@gmail.com>
2026-05-20 14:29:12 +0530

    Add function to accept powerball Fix invalid input incase of alphabets

--- powerball/main.py
+++ powerball/main.py
@@ -8,11 +8,18 @@ def print_message(message):
 
 
 def get_input(prompt):
-    return input(prompt).strip()
+    return input(f"{prompt}\n> ").strip()
 
 
-def get_numbers(numbers):
-    return set(map(int, numbers.split()))
+def convert_to_int(number):
+    try:
+        return int(number)
+    except ValueError:
+        return 0
+
+
+def get_white_numbers(numbers):
+    return set(map(convert_to_int, numbers))
 
 
 def check_len(numbers):
@@ -26,28 +33,36 @@ def check_white_balls(numbers):
 def get_white_balls():
     while True:
         balls = get_input(
-            "Enter 5 different numbers from 1 to 69, with spaces between them \n (For Example: 6 23 34 49 68): "
+            "Enter 5 different numbers from 1 to 69, with spaces between them \n(For Example: 6 23 34 49 68)"
         )
-        numbers = get_numbers(balls)
-        if not check_len(numbers):
+        numbers = get_white_numbers(balls)
+        if 0 in numbers:
+            print_message("Enter numbers only!")
+        elif not check_len(numbers):
             print_message("Please enter 5 distinct numbers!")
-            continue
         elif not check_white_balls(numbers):
             print_message("Please select numbers from 1 to 69!")
-            continue
-        return numbers
+        else:
+            return numbers
 
 
-def check_red_ball(number):
+def check_powerball(number):
     return 1 <= number <= 26
 
 
-def get_red_ball():
-    pass
+def get_powerball():
+    while True:
+        number = convert_to_int(get_input("Enter Powerball number from 1 to 26"))
+        if number == 0:
+            print_message("Please enter a single number!")
+        elif check_powerball(number):
+            print_message("Please select numbers from 1 to 26!")
+        else:
+            return number
 
 
 # Driver Code
 
 print(logo)
-print(get_white_balls())
-get_red_ball()
+white_balls = get_white_balls()
+powerball = get_powerball()
