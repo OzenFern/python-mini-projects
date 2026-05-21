8d1377fc3e01e36dc7fec1a84b9f402de917c986
Ozen Fernandes <ozenfernandes@gmail.com>
2026-05-21 17:00:18 +0530

    Pb temp (#4)
    * Add Initial Files
    
    * Add function to accept white balls
    
    * Add function to accept powerball
    Fix invalid input incase of alphabets
    
    * Make convert_to_int return None on error, fix bugs
    
    * Refactor function to make it reusable and implement num_of_plays
    
    * Fix the sneaky set bug
    Rename functions to be more meaningful
    use is operator for comparison
    
    * Add app_state, Work in progress...
    
    * Add print_fee function
    
    Commit added from mobile.
    
    * Add print_fee function
    
    * Display total cost in driver code
    
    * Add winning condition check and prizes dictionary
    
    * Make a working draft of powerball
    
    * Make a working powerball game
    Set prizes to be int values only
    add end_game function
    calculate total profit and display result
    
    * Write clear and clean variable and function names
    
    * Optimise code
    
    * Squashed commit of the following:
    
    commit 3cc745a3a5518915cfbc9848eed742b7590a5da6
    Author: OzenFern <ozenfernandes@gmail.com>
    Date:   Thu May 21 12:51:45 2026 +0530
    
        Optimise code
    
    commit 546f6ec1830e9ec86816640e43ca9cadb4f13db1
    Author: OzenFern <ozenfernandes@gmail.com>
    Date:   Thu May 21 12:19:56 2026 +0530
    
        Write clear and clean variable and function names
    
    commit d73ba2ce65f08ae819bf80c86816ae0c0913d013
    Author: OzenFern <ozenfernandes@gmail.com>
    Date:   Thu May 21 12:11:56 2026 +0530
    
        Make a working powerball game
        Set prizes to be int values only
        add end_game function
        calculate total profit and display result
    
    commit c5c1c74ad99e7c92a1442bff702100ce2a80bce4
    Author: OzenFern <ozenfernandes@gmail.com>
    Date:   Wed May 20 22:36:41 2026 +0530
    
        Make a working draft of powerball
    
    commit 99d9354e304da717bf92a938f49b5a87c03cb681
    Author: OzenFern <ozenfernandes@gmail.com>
    Date:   Wed May 20 22:05:05 2026 +0530
    
        Add winning condition check and prizes dictionary
    
    commit a1c9f34251478a1692540a9fb113e48d25996148
    Author: Ozen Fernandes <ozenfernandes@gmail.com>
    Date:   Wed May 20 19:54:47 2026 +0530
    
        Display total cost in driver code
    
    commit 68285de588c7d2fbbbbcc7bee453eae9d909aa1a
    Author: Ozen Fernandes <ozenfernandes@gmail.com>
    Date:   Wed May 20 19:52:27 2026 +0530
    
        Add print_fee function
    
    * Fix merge conflicts post merge
    
    * Remove redundant formatting
    
    * Working version of powerball with game_state
    
    * Improve the game
    
    * Fix logical and calulation bugs
    
    * Update input stmt
    
    * Improve the game end and print meaningful messages
    
    * Accept initial amount from player
    
    * Fix magic number logical bug
    
    * Remove redundant if-else block for max_attempts
    
    * Fix logical bugs and make print stmts readable
    
    * Add dynamic calculation of balance and fix print stmt
    
    * Update amount lost after attempts purchase
    
    * Fixed amount getting deducted on retry
    
    * Delete redundant function call
    
    * Improve print messages
    
    * Add README.md and rename the directory
    
    * Rename parent directory
    
    * Add finishing touches to the code

---
+++ powerball-simulator/main.py
@@ -0,0 +1,228 @@
+from ascii_art import logo
+from random import sample, randint
+
+PRIZES = {
+    (5, True): 1000000000,  # Jackpot placeholder
+    (5, False): 1000000,
+    (4, True): 50000,
+    (4, False): 100,
+    (3, True): 100,
+    (3, False): 7,
+    (2, True): 7,
+    (2, False): 0,
+    (1, True): 4,
+    (1, False): 0,
+    (0, True): 4,
+    (0, False): 0,
+}
+
+game_state = {
+    "cost_per_attempt": 2,
+    "initial_amount": 0,
+    "max_initial_amount": 1000,
+    "attempts": 0,
+    "amount_earned": 0,
+    "amount_lost": 0,
+}
+
+# Function Definition
+
+
+def print_message(message):
+    print(f"\n\t{message}\n")
+
+
+def get_input(prompt):
+    return input(f"{prompt}\n> ").strip()
+
+
+def convert_to_int(number):
+    try:
+        return int(number)
+    except ValueError:
+        return None
+
+
+def parse_white_numbers(raw_input):
+    return list(map(convert_to_int, raw_input.split()))
+
+
+def has_five_numbers(numbers):
+    return len(numbers) == 5
+
+
+def has_unique_numbers(numbers):
+    return len(set(numbers)) == 5
+
+
+def check_white_balls(numbers):
+    return all(1 <= x <= 69 for x in numbers)
+
+
+def get_white_balls():
+    while True:
+        balls = get_input(
+            "Enter 5 different numbers from 1 to 69, with spaces between them \n(For Example: 6 23 34 49 68)"
+        )
+        numbers = parse_white_numbers(balls)
+        if None in numbers:
+            print_message("Only numbers and spaces are allowed!")
+        elif not has_five_numbers(numbers):
+            print_message("Please enter exactly 5 numbers!")
+        elif not has_unique_numbers(numbers):
+            print_message("Please enter 5 distinct numbers!")
+        elif not check_white_balls(numbers):
+            print_message("Please select numbers from 1 to 69!")
+        else:
+            return numbers
+
+
+def num_is_between(num_to_check, from_num, to_num):
+    return from_num <= num_to_check <= to_num
+
+
+def get_single_number(message, from_num, to_num):
+    while True:
+        number = convert_to_int(get_input(message))
+        if number is None:
+            print_message("Please enter a single number!")
+        elif not num_is_between(number, from_num, to_num):
+            print_message(f"Please select numbers from {from_num} to {to_num}!")
+        else:
+            return number
+
+
+def draw_white_balls():
+    return sample(range(1, 70), k=5)
+
+
+def draw_powerball():
+    return randint(1, 26)
+
+
+def play_powerball(user_white_balls, user_powerball):
+    winning_white_balls = draw_white_balls()
+    winning_powerball = draw_powerball()
+
+    white_matches = len((set(user_white_balls) & set(winning_white_balls)))
+    powerball_match = user_powerball == winning_powerball
+
+    prize = PRIZES[(white_matches, powerball_match)]
+
+    prize_text = "No win" if prize == 0 else f"${prize:,}"
+
+    white_balls_str = ", ".join(map(str, sorted(winning_white_balls)))
+    print(
+        f"The winning numbers are: {white_balls_str:<20} Powerball: {winning_powerball:<5} Prize: {prize_text:<15}"
+    )
+    return prize
+
+
+def end_round_message(total_cost, prize):
+    total_winnings = sum(prize)
+    profit = total_winnings - total_cost
+    if total_winnings == 0:
+        print_message(f"You've wasted your ${total_cost:,} this round :(")
+    elif profit < 0:
+        print_message(f"You've lost ${-profit:,} this round, but not all your money :|")
+    else:
+        print_message(
+            f"Congrats! You've won ${total_winnings:,} and got a profit of ${profit:,} :)"
+        )
+    game_state["amount_earned"] += total_winnings
+
+
+def possible_attempts():
+    return calculate_total_amount() // game_state["cost_per_attempt"]
+
+
+def calculate_total_amount():
+    return (
+        game_state["initial_amount"]
+        + game_state["amount_earned"]
+        - game_state["amount_lost"]
+    )
+
+
+def calculate_fee(attempts, cost=game_state["cost_per_attempt"]):
+    total_cost = attempts * cost
+    return total_cost
+
+
+def print_fee(attempts, total_cost):
+    print_message(
+        f"It costs ${total_cost:,} to play for {attempts} time{'s' if attempts != 1 else ''}, "
+        "But don't worry, I'm sure you'll win it all back."
+    )
+
+
+def start_round():
+    while True:
+        max_attempts = possible_attempts()
+        attempts = get_single_number(
+            f"How many times do you want to play? (Max: {max_attempts})",
+            1,
+            max_attempts,
+        )
+        total_cost = calculate_fee(attempts)
+        print_fee(attempts, total_cost)
+        proceed = get_input(
+            "Press Enter to begin or type 'r' to re-enter attempts..."
+        ).lower()
+        if proceed == "r":
+            print_message("Reverting...")
+            continue
+        game_state["amount_lost"] += total_cost
+        return attempts, total_cost
+
+
+def can_play_game():
+    return calculate_total_amount() >= game_state["cost_per_attempt"]
+
+
+def print_user_funds():
+    print_message(f"""
+    {'Funds Status'.center(20, "-")}
+    Amount Earned: ${game_state['amount_earned']:,}
+    Amount Lost:   ${game_state['amount_lost']:,}
+    Balance:       ${calculate_total_amount():,}
+    """)
+
+
+def powerball_app():
+    white_balls = get_white_balls()
+    powerball = get_single_number("Enter Powerball number from 1 to 26", 1, 26)
+    attempts, total_cost = start_round()
+    prizes = [play_powerball(white_balls, powerball) for _ in range(attempts)]
+    end_round_message(total_cost, prizes)
+    game_state["attempts"] += attempts
+
+
+def continue_to_play():
+    proceed = get_input("Would you like to play again? (y/n)").lower()
+    if proceed != "y":
+        print_message("Goodbye, hope to see you again...")
+        quit()
+
+
+# Driver Code
+
+print(logo)
+game_state["initial_amount"] = get_single_number(
+    f"Welcome Player!\n How much money would you like to start with? (Enter between ${game_state['cost_per_attempt']} to ${game_state['max_initial_amount']})",
+    game_state["cost_per_attempt"],
+    game_state["max_initial_amount"],
+)
+print_message(f"Balance of ${game_state['initial_amount']} added!")
+while can_play_game():
+    powerball_app()
+    print_user_funds()
+    continue_to_play()
+
+print_message("Sadly, you're out of funds!")
+see_attempts = get_input(
+    "Would you like to see your number of attempts for powerball? (y/n)"
+)
+if see_attempts == "y":
+    print_message(f"Total_Attempts: {game_state['attempts']}")
+print("See you again...")
