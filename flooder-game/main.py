190c52b133572eb656386c28bed1ec14da110ddb
OzenFern <ozenfernandes@gmail.com>
2026-05-24 15:19:37 +0530

    Add function to ask player to select a colour

--- flooder-game/main.py
+++ flooder-game/main.py
@@ -1,13 +1,21 @@
-import bext, random
+import bext, random, sys
 
 # TODO:
 # Make width & height dynamic
 # Dynamic moves per game
 # Add difficulty levels
+# Add bg("Black") in driver instead of function
 
-game_state = {
-    "tile_colours": ("red", "green", "blue", "purple", "yellow", "cyan"),
-    "width": 12,
+game_state: dict = {
+    "tile_colours": {
+        "R": "red",
+        "G": "green",
+        "B": "blue",
+        "P": "purple",
+        "Y": "yellow",
+        "C": "cyan",
+    },
+    "width": 20,
     "height": 12,
     "moves_per_game": 20,
     "line": chr(9472),
@@ -20,17 +28,18 @@ game_state = {
 }
 
 
-def get_new_board():
+def get_new_board() -> dict:
     """Creates a new board"""
     board = {}
     for x in range(game_state["width"]):
         for y in range(game_state["height"]):
-            board[(x, y)] = random.choice(game_state["tile_colours"])
+            colours = tuple(game_state["tile_colours"].values())
+            board[(x, y)] = random.choice(colours)
     return board
 
 
-def smear_colours(board, level=1):
-    """Smear the colours horizontally, by making two neighbouring blocks the same color"""
+def smear_colours(board: dict, level: int = 1) -> dict:
+    """Smear the colours vertically, by making two neighbouring blocks the same color"""
     for _ in range(0, game_state["width"] * game_state["height"], level):
         x = random.randrange(0, game_state["width"])
         y = random.randrange(1, game_state["height"])
@@ -43,7 +52,7 @@ def default_colours():
     bext.bg("black")
 
 
-def display_board(board):
+def display_board(board: dict):
     """Display colourful board"""
 
     # Draw the top horizontal line
@@ -78,8 +87,44 @@ def display_board(board):
     )
 
 
+def ask_player_for_colour() -> str:
+    """Controls which colour is applied at the top left tile"""
+    while True:
+        bext.fg("white")
+        print("Choose one of ", end="")
+
+        for colour in game_state["tile_colours"].values():
+            bext.fg(colour)
+            print(f"({colour[0].upper()}){colour[1:]}", end=", ")
+        choice = input("or (Q)uit: ").upper()
+
+        if choice == "Q":
+            exit_game()
+        elif choice in game_state["tile_colours"]:
+            return game_state["tile_colours"][choice]
+        else:
+            print(
+                "Invalid choice. Please select the first letter of the colour you want to apply..."
+            )
+
+
+def apply_color():
+    pass
+
+
+def exit_game() -> None:
+    """Exits the game and resets the terminal colours to default"""
+    try:
+        sys.exit()
+    finally:
+        bext.fg("reset")
+        bext.bg("reset")
+        print("Thanks for playing...")
+
+
 # Driver
 moves_left = game_state["moves_per_game"]
 new_board = get_new_board()
 board = smear_colours(new_board)
 display_board(board)
+colour = ask_player_for_colour()
